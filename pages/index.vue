2c4c99a350325b9822171e56368450f77af4af21
Darlington <fritzdultimate@gmail.com>
2022-07-30 23:12:52 +0100

    fontawesome

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="text-6xl text-purple-500 font-bold">This is a home page now working</div>
+  <div class="relative border w-full">
+    <button
+      class="
+        absolute
+        top-0
+        right-0
+        mx-5
+        my-10
+        rounded
+        bg-purple-700
+        text-white text-base
+        font-medium
+        px-5
+        py-3
+        hover:opacity-80
+        focus:bg-purple-900
+      "
+    >
+      Add Record
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
