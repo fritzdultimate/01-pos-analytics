28503785493c3aeaa17b886e3498bb677ff2afee
Darlington <fritzdultimate@gmail.com>
2022-07-30 21:57:09 +0100

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-
-})
+export default {
+  buildModules: [
+    '@nuxt/postcss8',
+    // ...
+  ],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  css: ['~/assets/css/main.css'],
+}
